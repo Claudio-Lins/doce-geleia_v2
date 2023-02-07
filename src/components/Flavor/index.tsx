42b8d82e63313da4414bf509483ee076b0ebce3e
Claudio Lins <claudio.lins@me.com>
2023-02-07 22:16:07 +0000

    orderby id asc

--- src/components/Flavor/index.tsx
+++ src/components/Flavor/index.tsx
@@ -1,43 +1,48 @@
-import { ProductDetailsProps } from "@/@types/ProductDetailsProps";
-import { ProductProps } from "@/@types/ProductProps";
-import { FlavorCard } from "./FlavorCard";
+import { ProductDetailsProps } from '@/@types/ProductDetailsProps'
+import { ProductProps } from '@/@types/ProductProps'
+import { FlavorCard } from './FlavorCard'
 
 interface FlavorProps {
-  products: any;
-  productDetails: any;
+  products: any
+  productDetails: any
 }
 
-export function Flavor({products, productDetails}: FlavorProps) {
+export function Flavor({ products, productDetails }: FlavorProps) {
   return (
-    <div id="sabores" className='flex flex-col items-center w-full mx-auto mb-10 max-w-7xl md:h-[calc(100vh-150px)] md:scroll-mt-32'>
-      <h2 className='font-Montserrat font-bold text-3xl mb-2'>Sabores</h2>
-      <hr className="w-full mb-8" />
-      <div className='flex flex-wrap justify-evenly md:justify-center md:gap-20 gap-4 items-center w-full h-auto md:h-[80%]'>
+    <div
+      id='sabores'
+      className='mx-auto mb-10 flex w-full max-w-7xl flex-col items-center md:h-[calc(100vh-150px)] md:scroll-mt-32'
+    >
+      <h2 className='mb-2 font-Montserrat text-3xl font-bold'>Sabores</h2>
+      <hr className='mb-8 w-full' />
+      <div className='flex h-auto w-full flex-wrap items-center justify-evenly gap-4 md:h-[80%] md:justify-center md:gap-20'>
         {products.map((product: ProductProps) => {
-        return (
-          <div key={product.id}>
-            <FlavorCard
-              title={product.title}
-              src={product.coverUrl}
-              alt={product.title}
-              details={productDetails
-              .filter((productDetail: ProductDetailsProps) => productDetail.productId === product.id)
-                .map((productDetail: ProductDetailsProps) => {
-                return (
-                  <div key={productDetail.id}>
-                    <div className="flex text-[8px]">
-                    {productDetail.weight}
-                    {productDetail.price}
-                    </div>
-                  </div>
-                )
-              })}
+          return (
+            <div key={product.id}>
+              <FlavorCard
+                title={product.title}
+                src={product.coverUrl}
+                alt={product.title}
+                details={productDetails
+                  .filter(
+                    (productDetail: ProductDetailsProps) =>
+                      productDetail.productId === product.id
+                  )
+                  .map((productDetail: ProductDetailsProps) => {
+                    return (
+                      <div key={productDetail.id}>
+                        <div className='flex text-[8px]'>
+                          {productDetail.weight}
+                          {productDetail.price}
+                        </div>
+                      </div>
+                    )
+                  })}
               />
-          </div>
-        )
-      })}
+            </div>
+          )
+        })}
       </div>
     </div>
   )
-
 }
--- src/services/getProductDetail.ts
+++ src/services/getProductDetail.ts
@@ -1,6 +1,10 @@
 import { prisma} from '@/lib/prisma'
 
 export async function getProductDetail() {
-  const productDeatils = await prisma.productDetail.findMany({})
+  const productDeatils = await prisma.productDetail.findMany({
+    orderBy: {
+      id: 'asc',
+    }
+  })
   return productDeatils
 }
